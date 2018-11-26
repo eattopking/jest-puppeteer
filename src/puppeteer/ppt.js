e44cf9353158459b749e69d2c7ad28f1fbdb6089
zhanghengh <zhanghengh@yonyou.com>
2018-11-27 00:15:27 +0800

    可以跑起来了哈哈

--- src/puppeteer/ppt.js
+++ src/puppeteer/ppt.js
@@ -1,12 +1,12 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('http://localhost:3000/#/singleTable');
-  await page.click('#editButton')
-  await page.click('.edit-table-edit-td')
-  await page.click('.multi-lang-text-item')
-  await page.type('.nc-input', "66666", {delay: 500});
-  await page.click('.u-table-body')
-})()
+  await page.goto("http://localhost:3000/#/singleTable");
+  await page.click("#editButton");
+  await page.click(".edit-table-edit-td");
+  await page.click(".multi-lang-text-item");
+  await page.type(".nc-input", "66666", { delay: 500 });
+  await page.click(".u-table-body");
+})();
