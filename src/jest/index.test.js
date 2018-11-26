ea7553a29bbbe0b3e16eccc4048740336a17d953
zhanghengh <zhanghengh@yonyou.com>
2018-11-26 14:37:45 +0800

    提交测试项目

---
+++ src/jest/index.test.js
@@ -0,0 +1,27 @@
+
+import React, { Component } from 'react';
+import Enzyme, { shallow }  from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'; // react16适配器
+import toJson from "enzyme-to-json"; // 引入enzyme-to-json为快照提供了json的组件格式
+import Jest from './demo/jest/jest';
+
+Enzyme.configure({ adapter: new Adapter() }); //适配react16
+
+describe("这是一组测试用例", () => {
+  it('Enzyme测试组件', () => { // Enzyme测试用例
+    let wrapper = shallow(<Jest/>);
+    expect(wrapper.add(3,5)).toBe(8);
+  })
+  test('2+2是否是4呢', () => { // jest测试用例
+    expect(4).toBe(4);
+  });
+  test('2+5是否是5呢', () => {
+    expect(2+3).toBe(5);
+  });
+  test('2+4是否是6呢', () => {
+    expect(2+4).toBe(6);
+  });
+});
+
+
+
