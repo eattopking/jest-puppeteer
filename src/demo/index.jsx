6836368e4edc055ca1ebe34530b5e6f396d2cd7d
zhanghengh <zhanghengh@yonyou.com>
2018-11-27 23:25:42 +0800

    添加依赖，增加代码

--- src/demo/index.jsx
+++ src/demo/index.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 export default class Test extends Component {
+  constructor(props, nextContext) {
+    super();
+    this.state = { data: "测试" };
+  }
   render() {
     return <div>11111111</div>;
   }
--- src/jest/index.test.js
+++ src/jest/index.test.js
@@ -1,6 +1,7 @@
 // 自动逻辑测试
 
 import React from "react";
+import chalk, { red, yellow, green, white } from "chalk";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16"; // react16适配器
 import toJson from "enzyme-to-json"; // 引入enzyme-to-json为快照提供了json的组件格式
@@ -9,18 +10,19 @@ import Jest from "../demo";
 Enzyme.configure({ adapter: new Adapter() }); //适配react16
 
 describe("这是一组测试用例", () => {
-  it("Enzyme测试组件", () => {
+  it(red("Enzyme测试组件"), () => {
     // Enzyme测试用例
     let wrapper = shallow(<Jest />);
+    console.log("state", wrapper.state());
   });
-  test("2+2是否是4呢", () => {
+  test(white("2+2是否是4呢"), () => {
     // jest测试用例
     expect(4).toBe(4);
   });
-  test("2+5是否是5呢", () => {
+  test(yellow("2+5是否是5呢"), () => {
     expect(2 + 3).toBe(5);
   });
-  test("2+4是否是6呢", () => {
+  test(green("2+4是否是6呢"), () => {
     expect(2 + 4).toBe(6);
   });
 });
