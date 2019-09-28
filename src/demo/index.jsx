d51540b1c78a47623defdb9f1dfd20538f683a35
zhanghengh <zhanghengh@yonyou.com>
2019-09-28 23:19:26 +0800

    优化代码

--- src/demo/index.jsx
+++ src/demo/index.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 
 export default class Test extends Component {
-  constructor(props, nextContext) {
-    super();
+  constructor(props) {
+    super(props);
     this.state = { data: "测试" };
   }
   render() {
-    return <div>11111111</div>;
+    return <div>测试页面</div>;
   }
 }
