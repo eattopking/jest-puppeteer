baa64ea19ea20fc28282bcadd1631081accbae03
zhangheng05 <zhangheng05@baijiahulian.com>
2021-01-06 15:12:19 +0800

    更新

--- src/puppeteer/ppt.js
+++ src/puppeteer/ppt.js
@@ -5,10 +5,6 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("http://localhost:3000/#/singleTable");
-  await page.click("#editButton");
-  await page.click(".edit-table-edit-td");
-  await page.click(".multi-lang-text-item");
-  await page.type(".nc-input", "66666", { delay: 500 });
-  await page.click(".u-table-body");
+  await page.goto("https://www.baidu.com/");
+  await page.click("#su");
 })();
